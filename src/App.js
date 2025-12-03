5b7f5dbf72e90176d91147d25b6de9f169694e7b
ram14112003 <ram14112003@gmail.com>
2025-12-03 11:20:53 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white text-2xl font-bold">
-      React + Tailwind CSS App
+      Pcs office site
     </div>
   );
 }
