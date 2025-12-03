c9f0df840f8d11938404ee573b179f19168696bb
Subisha25 <subisha2002.m@gmail.com>
2025-12-03 11:09:26 +0530

    code pushing

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white text-2xl font-bold">
+      React + Tailwind CSS App
+    </div>
+  );
+}
+
+export default App;
